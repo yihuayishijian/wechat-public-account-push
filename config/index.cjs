848ca987211e0c666ddc51a27077714aed089c42
无敌小小硕 <72974446+yihuayishijian@users.noreply.github.com>
2023-04-02 23:36:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,8 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olldg58ytcDi7texZU0B3_njpQKg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'k9mZ14aShO7LCgvL7UM8Kej8gfUw3UGE5YEGX9laOgs',
+      //useTemplateId: 'k9mZ14aShO7LCgvL7UM8Kej8gfUw3UGE5YEGX9laOgs',
+      useTemplateId: 'yP6wWzUFdqok-svME-H5-ZNamsAiO0ipsMgPqQGPHqw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-13',
       festivals: [
@@ -49,7 +50,8 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'k9mZ14aShO7LCgvL7UM8Kej8gfUw3UGE5YEGX9laOgs',
+  //CALLBACK_TEMPLATE_ID: 'k9mZ14aShO7LCgvL7UM8Kej8gfUw3UGE5YEGX9laOgs',
+  CALLBACK_TEMPLATE_ID: 'yP6wWzUFdqok-svME-H5-ZNamsAiO0ipsMgPqQGPHqw',
 
   CALLBACK_USERS: [
     {
